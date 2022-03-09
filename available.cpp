4f4d787ffe1d829264d5f9ab8132cbb3cbc32ee8
taonyzeng <52616901+taonyzeng@users.noreply.github.com>
2022-03-09 16:50:05 -0500

    implemented all of the analysis

--- available.cpp
+++ available.cpp
@@ -15,45 +15,184 @@ using namespace llvm;
 using namespace std;
 
 namespace {
+
+  class Analysis : public Dataflow {
+      public:
+        Analysis (Direction direction, MeetOp meetop, Domain domain)
+            : Dataflow (direction, meetop, domain) {}
+        
+        TransferOutput transferFn (VSet input, BasicBlock *curr) {
+            TransferOutput output;
+            VSet gen, kill;
+            for (auto inst = curr->begin(); inst != curr->end(); ++inst) {
+                std::string val;
+                if (isa<Instruction> (&*inst)) {
+                    val = getShortValueName(&*inst);
+                }
+
+                if (auto load_ins = dyn_cast<StoreInst>(&*inst)) {
+                    val = getShortValueName(load_ins->getPointerOperand());
+                }
+                
+                // skip if it's not a value
+                if (val == "") {
+                    continue;
+                }
+
+                // if input has be re-assigned, kill it
+                for (auto LHS : input) {
+                    Expression* exp = (Expression *) domain[LHS];
+                    std::string left = getShortValueName(exp->v1), right = getShortValueName(exp->v2);
+                    if (left == val || right == val) {
+                        kill.insert(LHS);
+                    }    
+                }
+
+                // some values are killed in the same Basic Block, so sad
+                auto tmp = gen.end();
+                for (auto i = gen.begin(); i != gen.end(); ++i) {
+                    if (tmp != gen.end()) {
+                        gen.erase(tmp);
+                        tmp = gen.end();
+                    }
+                    Expression* exp = (Expression *) domain[*i];
+                    std::string left = getShortValueName(exp->v1), right = getShortValueName(exp->v2);                        
+                    if (left == val || right == val) {
+                        tmp = i;
+                    }
+                }
+
+                // insert to generate set
+                if (isa<BinaryOperator> (&*inst)) {
+                    Expression exp = Expression(&*inst);
+                    int i = 0;
+                    for (auto e : domain) {
+                        if (exp == *(Expression *)(e)) {
+                            break;
+                        }
+                        i++;
+                    }
+
+                    if (i < domain.size()) {
+                        gen.insert(i);
+                    }
+                }
+            }
+
+            auto tmp = substractSet(input, kill);
+            output.transfer = unionSet(tmp, gen);
+            return output;
+        }
+  };
+
   class AvailableExpressions : public FunctionPass {
     
-  public:
-    static char ID;
-    
-    AvailableExpressions() : FunctionPass(ID) { }
-    
-    virtual bool runOnFunction(Function& F) {
+    public:
+      static char ID;
       
-      // Here's some code to familarize you with the Expression
-      // class and pretty printing code we've provided:
+      AvailableExpressions() : FunctionPass(ID) { }
       
-      vector<Expression> expressions;
-      for (Function::iterator FI = F.begin(), FE = F.end(); FI != FE; ++FI) {
-	       BasicBlock* block = &*FI;
-	       for (BasicBlock::iterator i = block->begin(), e = block->end(); i!=e; ++i) {
-            
-	         Instruction* I = &*i;
-	         // We only care about available expressions for BinaryOperators
-	         if (BinaryOperator *BI = dyn_cast<BinaryOperator>(I)) {
-	           
-	           expressions.push_back(Expression(BI));
-	         }
-	       }
+      virtual bool runOnFunction(Function& F) {
+
+            outs() << "Function: " << F.getName() << "\n";
+            DataFlowResult result;
+            Domain domain;
+
+            // we only evaluate Expressions
+            for (auto I = inst_begin(F); I != inst_end(F); ++I) {
+                if (isa<BinaryOperator> (*I)) {
+                    domain.push_back(new Expression(&*I));
+                }
+            }
+
+            Analysis analysis = Analysis(Direction::FORWARD, MeetOp::INTERSECT,  domain);
+            result = analysis.run(F, VSet(), VSet());
+
+            // output the result
+            int index = 0;
+            for (auto &BB : F) {
+                outs() << "\n<" << BB.getName() << ">\n";
+                VSet gen = result.result[&BB].in;
+                for (auto &I : BB) {
+                    outs() << index << ": " << I;
+
+                    std::string val = getShortValueName(&I);
+                    
+                    // killed redefined expressions
+                    auto tmp = gen.end();
+                    for (auto i = gen.begin(); i != gen.end(); ++i) {
+                        if (tmp != gen.end()) {
+                            gen.erase(tmp);
+                            tmp = gen.end();
+                        }
+                        Expression* exp = (Expression *) domain[*i];
+                        std::string left = getShortValueName(exp->v1), right = getShortValueName(exp->v2);                        
+                        if (left == val || right == val) {
+                            tmp = i;
+                        }
+                    }
+                    
+                    // insert expressions
+                    if (isa<BinaryOperator> (&I)) {
+                        Expression exp = Expression(&I);
+                        int i = 0;
+                        for (auto e : domain) {
+                            if (exp == *(Expression *)(e)) {
+                                break;
+                            }
+                            i++;
+                        }
+                        gen.insert(i);
+                    }
+                    
+                    // pretty print
+                    outs() << "\t{";
+                    for (auto i : gen) {
+                        outs() << ((Expression *) domain[i])->toString() << ", ";
+                    }
+                    outs() << "}\n";
+
+                    index++;
+                }    
+            }
+            // clean up
+            for (auto exp : domain) {
+                delete (Expression *) exp;
+            }
+
+            return false;
       }
       
-      // Print out the expressions used in the function
-      outs() << "Expressions used by this function:\n";
-      printSet(&expressions);
+      virtual void getAnalysisUsage(AnalysisUsage& AU) const {
+        AU.setPreservesAll();
+        
+        // Here's some code to familarize you with the Expression
+        // class and pretty printing code we've provided:
+        
+        /*vector<Expression> expressions;
+        for (Function::iterator FI = F.begin(), FE = F.end(); FI != FE; ++FI) {
+           BasicBlock* block = &*FI;
+           for (BasicBlock::iterator i = block->begin(), e = block->end(); i!=e; ++i) {
+              
+             Instruction* I = &*i;
+             // We only care about available expressions for BinaryOperators
+             if (BinaryOperator *BI = dyn_cast<BinaryOperator>(I)) {
+               
+               expressions.push_back(Expression(BI));
+             }
+           }
+        }
+        
+        // Print out the expressions used in the function
+        outs() << "Expressions used by this function:\n";
+        printSet(&expressions);
+        
+        // Did not modify the incoming Function.
+        return false;*/
+      }
       
-      // Did not modify the incoming Function.
-      return false;
-    }
-    
-    virtual void getAnalysisUsage(AnalysisUsage& AU) const {
-      AU.setPreservesAll();
-    }
-    
-  private:
+    private:
+
   };
   
   char AvailableExpressions::ID = 0;
--- reaching.cpp
+++ reaching.cpp
@@ -8,28 +8,124 @@
 #include "llvm/Support/raw_ostream.h"
 
 #include "dataflow.h"
+#include "available-support.h"
 
 using namespace llvm;
 
 namespace {
 
+  class Analysis : public Dataflow {
+      public:
+        Analysis (Direction direction, MeetOp meetop, Domain domain)
+            : Dataflow (direction, meetop, domain) {}
+        
+        TransferOutput transferFn (VSet input, BasicBlock *curr) {
+            TransferOutput output;
+            VSet gen, kill;
+            for (auto inst = curr->begin(); inst != curr->end(); ++inst) {
+                std::string val = "";
+                if (isa<Instruction> (&*inst)) {
+                    val = getShortValueName(&*inst);
+                }
+
+                if (auto load_ins = dyn_cast<StoreInst>(&*inst)) {
+                    val = getShortValueName(load_ins->getPointerOperand());
+                }
+
+                if (val == "") {
+                    continue;
+                }
+
+                // if input has be re-assigned, kill it
+                for (auto LHS : input) {
+                    if (isa<Instruction> (&*inst)) {
+                        if (getShortValueName((Value *) domain[LHS]) == val) {
+                            kill.insert(LHS);
+                        }
+                    }      
+                }
+
+                // some values are killed in the same Basic Block, so sad
+                auto tmp = gen.end();
+                for (auto i = gen.begin(); i != gen.end(); ++i) {
+                    if (tmp != gen.end()) {
+                        gen.erase(tmp);
+                        tmp = gen.end();
+                    }
+                    if (getShortValueName((Value *) domain[*i]) == val) {
+                        tmp = i;
+                    }
+                }
+
+                Index i = domainIndex(&*inst);
+                if (i != INDEX_NOT_FOUND) {
+                    gen.insert(i);
+                }
+            }
+
+            // (input - kill) U gen  
+            VSet tmp = substractSet(input, kill);
+            output.transfer = unionSet(tmp, gen);
+            return output;
+        }
+  };
+
   class Reaching : public FunctionPass {
-  public:
-    static char ID;
+      public:
+        static char ID;
 
-    Reaching() : FunctionPass(ID) { }
+        Reaching() : FunctionPass(ID) { }
 
-    virtual bool runOnFunction(Function& F) {
+        virtual bool runOnFunction(Function& F) {
 
-      // Did not modify the incoming Function.
-      return false;
-    }
+          // Did not modify the incoming Function.
+          outs() << "Function: " << F.getName() << "\n";
+          DataFlowResult result;
+          Domain domain;
 
-    virtual void getAnalysisUsage(AnalysisUsage& AU) const {
-      AU.setPreservesAll();
-    }
+          VSet boudary, interior;
+          // use functions arguments to initialize
+          for (auto arg = F.arg_begin(); arg != F.arg_end(); ++arg) {
+              domain.push_back(&*arg);
+              boudary.insert(domain.size() - 1);
+          }
+          
+          // add remaining instruction
+          for (auto I = inst_begin(F); I != inst_end(F); ++I) {
+              if (Value* val = dyn_cast<Value> (&*I)) {
+                  if (getShortValueName(val) != "") {
+                      if (std::find(domain.begin(), domain.end(), val) == domain.end()) {
+                          domain.push_back(val);
+                      }
+                  }
+              }
+          }
 
-  private:
+          Analysis analysis  = Analysis(Direction::FORWARD, MeetOp::UNION, domain);
+          result = analysis.run(F, boudary, interior);
+          // output in/out
+          for (auto &BB : F) {
+              outs () << "\n<" << BB.getName() << ">\n";
+              outs () << "in: ";
+              for (auto i : result.result[&BB].in) {
+                  outs() << getShortValueName((Value *)analysis.domain[i]) << " ";
+              }
+              outs () <<"\nout: ";
+              for (auto i : result.result[&BB].out) {
+                  outs() << getShortValueName((Value *)analysis.domain[i]) << " ";
+              }            
+              outs () << "\n";    
+          }
+
+          return false;
+        }
+
+        virtual void getAnalysisUsage(AnalysisUsage& AU) const {
+          AU.setPreservesAll();
+        }
+
+      private:
+  
   };
 
   char Reaching::ID = 0;
